c15e8a808bdd464f7e220d4e98e87fdb5cdb90b8
Jason-Waters <42689149+Jason-Waters@users.noreply.github.com>
2019-09-25 14:36:23 -0400

    8 - Object-Oriented Rooms

---
+++ Zork/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+    class Room
+    {
+        public override string ToString() => Name;
+        public string Name { get; }
+
+        public string Description { get; set; }
+
+        public Room(string name, string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
